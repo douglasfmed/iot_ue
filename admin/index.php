7ae5d82bde64371aaef7d7c9561357dc52d09c01
Douglas Medeiros <ddfmed@gmail.com>
2019-07-25 08:34:42 -0300

    Separando mais algumas coisas

--- admin/index.php
+++ admin/index.php
@@ -20,18 +20,18 @@
 <title>Plataforma IoT µE - Universidade Federal da Paraíba</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="apple-mobile-web-app-capable" content="yes">
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<link href="../css/bootstrap-responsive.min.css" rel="stylesheet">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
         rel="stylesheet">
-<link href="css/font-awesome.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
-<link href="css/pages/dashboard.css" rel="stylesheet">
-<link href="css/pages/signin.css" rel="stylesheet" type="text/css">
+<link href="../css/font-awesome.css" rel="stylesheet">
+<link href="../css/style.css" rel="stylesheet">
+<link href="../css/pages/dashboard.css" rel="stylesheet">
+<link href="../css/pages/signin.css" rel="stylesheet" type="text/css">
 <link rel="icon" href="img/favicon.ico">
-<script src="js/highcharts/highcharts.js"></script>
-<script src="js/highcharts/exporting.js"></script>
-<script src="js/highcharts/export-data.js"></script>
+<script src="../js/highcharts/highcharts.js"></script>
+<script src="../js/highcharts/exporting.js"></script>
+<script src="../js/highcharts/export-data.js"></script>
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -105,11 +105,11 @@ include_once("menu.php");
 <!-- Le javascript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="js/jquery-1.7.2.min.js"></script>
-<script src="js/excanvas.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/base.js"></script>
-<script type="text/javascript" src="js/script_buscas.js"></script>
+<script src="../js/jquery-1.7.2.min.js"></script>
+<script src="../js/excanvas.min.js"></script>
+<script src="../js/bootstrap.js"></script>
+<script src="../js/base.js"></script>
+<script type="text/javascript" src="../js/script_buscas.js"></script>
 
 </body>
 </html>
